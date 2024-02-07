0a6e86514e91a0282ea00ea52e960f5a05957f02
Florian MEUNIER <Florian.MEUNIER@ofracar.fr>
2024-02-07 11:16:06 +0100

    Switch jours/semaine/mois (pas encore en lien avec la chart)

--- src/modules/shared/Chart.js
+++ src/modules/shared/Chart.js
@@ -8,23 +8,26 @@ import {
     StackedBarChart
   } from "react-native-chart-kit";
 
-  const screenWidth = Dimensions.get("window").width*0.;
+  
+  
   const chartConfig = {
     backgroundGradientFrom: "#0000",
     backgroundGradientFromOpacity: 0,
     backgroundGradientTo: "#0000",
     backgroundGradientToOpacity: 0,
-    color: (opacity = 1) => `rgba(0, 180, 236, ${opacity})`,
+    color: (opacity = 1) => `rgba(0, 180, 250, ${opacity})`,
     strokeWidth: 2, // optional, default 3
     barPercentage: 0.5,
-    useShadowColorFromDataset: false // optional
+    useShadowColorFromDataset: false ,// optional
+    
+    
   };
 
   const data = {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["January", "February", "March", "April", "May"],
     datasets: [
       {
-        data: [20, 45, 28, 80, 99, 43],
+        data: [2500, 5000, 7300, 9800, 4300, 10000],
         color: (opacity = 1) => `rgba(0, 180, 236, ${opacity})`, // optional
         strokeWidth: 2 // optional
       }
@@ -33,14 +36,18 @@ import {
   };
 export default function Chart() {
   return (
-    <LineChart
+      <LineChart
         data={data}
-        width={screenWidth}
-        height={130}
-        verticalLabelRotation={30}
+        width={Dimensions.get('window').width * 1}
+        height={170}
+        withDots= {false}
         chartConfig={chartConfig}
         bezier
-    />
+        style={{
+          borderRadius: 16,
+        }}
+      />
+    
   )
 }
 
