8f67944cb3bd3510d748e264d8e83a13bb38ea3c
Ecaron76 <ecaronpro76@gmail.com>
2024-03-02 19:47:32 +0100

    Création du service StepsChallenge    Méthode:          -GetAllSteps          -GetDaySteps          -GetWeekSteps          -GetMonthSteps
    Affichage du nombre de pas dans le composant indicator de StatsPage

--- src/modules/StatsPage/StatsPage.js
+++ src/modules/StatsPage/StatsPage.js
@@ -1,9 +1,74 @@
 import {View, Button, Text} from 'react-native';
+import { StepsChallengeService } from '../../services/StepsChallengeService/StepsChallengeService';
+import { useIsFocused } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import Indicator from '../shared/Indicator';
 
 export default function StatsPage() {
+    const [allSteps, setAllSteps] = useState();
+    const [daySteps, setDaySteps] = useState();
+    const [weekSteps, setWeekSteps] = useState();
+    const [monthSteps, setMonthSteps] = useState();
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+        if(isFocused){ 
+            getAllSteps()
+            getWeekSteps()
+            getDaySteps()
+            getMonthSteps()
+        }
+    }, [isFocused]);
+    const getAllSteps = async () => {
+
+        // stepsData est un tableau qui contient 3 tableaux => 1- les 5 derniers mois / 2- les 5 dernières semaines / 3- les 5 derniers jours. 
+        const stepsData = await StepsChallengeService.getAllSteps();
+
+        // Contient le tableau de données necessaire pour remplir le composant graphique de pas. C'est un tableau qui contient trois tableau. 
+        //1) les mois 2) les semaines 3) les jours.
+        
+        setAllSteps(stepsData)
+    }; 
+    const getWeekSteps = async () => {
+
+        // stepsData est un tableau qui contient 3 tableaux => 1- les 5 derniers mois / 2- les 5 dernières semaines / 3- les 5 derniers jours. 
+        const stepsData = await StepsChallengeService.getWeeklySteps();
+
+        // Contient le tableau de données necessaire pour remplir le composant graphique de pas. C'est un tableau qui contient trois tableau. 
+        //1) les mois 2) les semaines 3) les jours.
+        
+        setWeekSteps(stepsData)
+    }; 
+    const getDaySteps = async () => {
+
+        // stepsData est un tableau qui contient 3 tableaux => 1- les 5 derniers mois / 2- les 5 dernières semaines / 3- les 5 derniers jours. 
+        const stepsData = await StepsChallengeService.getDaySteps();
+
+        // Contient le tableau de données necessaire pour remplir le composant graphique de pas. C'est un tableau qui contient trois tableau. 
+        //1) les mois 2) les semaines 3) les jours.
+        
+        setDaySteps(stepsData)
+    }; 
+    const getMonthSteps = async () => {
+
+        // stepsData est un tableau qui contient 3 tableaux => 1- les 5 derniers mois / 2- les 5 dernières semaines / 3- les 5 derniers jours. 
+        const stepsData = await StepsChallengeService.getMonthSteps();
+
+        // Contient le tableau de données necessaire pour remplir le composant graphique de pas. C'est un tableau qui contient trois tableau. 
+        //1) les mois 2) les semaines 3) les jours.
+        
+        setMonthSteps(stepsData)
+    }; 
+    
     return (
-        <View>
-         <Text>StatsPage</Text>
+        <View style={{width: '100%', height: '100%', padding:10}}>
+            <Text style={{fontSize:25, fontWeight: 'bold', textAlign:'center', marginBottom:20}}> Accomplissement du challenge</Text>
+            <View style={{gap:20}}>
+                <Indicator textIndicator="pas totaux" valueIndicator={allSteps}/>
+                <Indicator textIndicator="pas ce mois-ci" valueIndicator={monthSteps}/>
+                <Indicator textIndicator="pas cette semaine" valueIndicator={weekSteps}/>
+                <Indicator textIndicator="pas aujourd'hui" valueIndicator={daySteps}/>
+            </View>
         </View>
     );
 }
---
+++ src/services/StepsChallengeService/StepsChallengeService.js
@@ -0,0 +1,76 @@
+import { Pedometer } from "expo-sensors";
+import "react-native-url-polyfill/auto";
+import { supabase } from "../supabase/supabase";
+
+export const StepsChallengeService = {
+  getAllSteps: async function () {
+    let { data: daily_challenge_steps, error } = await supabase
+      .from("daily_user_steps")
+      .select("count");
+      daily_challenge_steps = daily_challenge_steps.reduce((sum, item) => sum + item.count, 0);
+      return daily_challenge_steps
+  },
+   
+  getDaySteps: async function () {
+    const today = new Date();
+  
+    let { data: daily_challenge_steps, error } = await supabase
+      .from("daily_user_steps")
+      .select("count")
+      .eq("date", today.toISOString().split("T")[0]);
+  
+    if (error) {
+      console.error("Erreur lors de la récupération des données :", error);
+      return null; 
+    }
+  
+    const totalStepsForToday = daily_challenge_steps.reduce((sum, item) => sum + item.count, 0);
+  
+    return totalStepsForToday;
+  },
+  
+  getWeeklySteps: async function () {
+    const today = new Date();
+    const startOfWeek = new Date(today);
+    startOfWeek.setHours(0, 0, 0, 0);
+    const endOfWeek = new Date(today);
+    endOfWeek.setDate(endOfWeek.getDate() + (6 - today.getDay()));
+    endOfWeek.setHours(23, 59, 59, 999);
+  
+    let { data: weekly_challenge_steps, error } = await supabase
+      .from("daily_user_steps")
+      .select("count")
+      .gte("date", startOfWeek.toISOString())
+      .lte("date", endOfWeek.toISOString());
+  
+    if (error) {
+      console.error("Erreur lors de la récupération des données :", error);
+      return null; 
+    }
+  
+    const totalStepsForWeek = weekly_challenge_steps.reduce((sum, item) => sum + item.count, 0);
+  
+    return totalStepsForWeek;
+  },
+   
+  getMonthSteps: async function () {
+    const today = new Date()
+    const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    console.log(firstDayOfMonth)
+  
+    let { data: monthly_challenge_steps, error } = await supabase
+      .from("daily_user_steps")
+      .select("count")
+      .gte("date", firstDayOfMonth.toISOString().split("T")[0])
+      .lte("date", today.toISOString().split("T")[0]);
+  
+    if (error) {
+      console.error("Erreur lors de la récupération des données :", error);
+      return null; 
+    }
+
+    const totalStepsForMonth = monthly_challenge_steps.reduce((sum, item) => sum + item.count, 0);
+  
+    return totalStepsForMonth;
+  }
+};
